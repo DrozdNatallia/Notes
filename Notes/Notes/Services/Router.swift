875c051fce30a950237268bbd5a376fe27375a2f
NatalliaDrozd <nat.dr000@gmail.com>
2022-10-19 22:47:39 +0300

    added Router

---
+++ Notes/Notes/Services/Router.swift
@@ -0,0 +1,53 @@
+//
+//  Router.swift
+//  Notes
+//
+//  Created by Natalia Drozd on 17.10.22.
+//
+
+import Foundation
+import UIKit
+protocol RouterMain {
+    var navigationController: UINavigationController? { get set }
+    var assemblyBuilder: AsselderBuildProtocol? { get set }
+}
+
+protocol RouterProtocol: RouterMain {
+    func initialViewController()
+    func showNotesViewController()
+  //  func popToRoot()
+}
+
+class Router: RouterProtocol {
+    var navigationController: UINavigationController?
+    var assemblyBuilder: AsselderBuildProtocol?
+    
+    init(navigationController: UINavigationController, assemblyBuilder: AsselderBuildProtocol) {
+        self.navigationController = navigationController
+        self.assemblyBuilder = assemblyBuilder
+    }
+    
+    // homeViewController
+    func initialViewController() {
+        if let navigationController = navigationController {
+            guard let homeViewController = assemblyBuilder?.createModel(router: self) else { return }
+            navigationController.viewControllers = [homeViewController]
+        }
+    }
+    
+    func showNotesViewController() {
+        if let navigationController = navigationController {
+            guard let notesViewController = assemblyBuilder?.createNotesModule(router: self) else {return}
+            navigationController.pushViewController(notesViewController, animated: true)
+        }
+    }
+}
+
+
+
+//func showFlightsModule(location: String, icon: UIImage?) {
+//    if let navigationController = navigationController {
+//        guard let flightsViewController = assemblyBuilder?.createFlightsModule(location: location, image: icon, router: self) else { return }
+//        navigationController.pushViewController(flightsViewController, animated: true)
+//    }
+//}
